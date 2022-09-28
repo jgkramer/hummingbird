206869ead39f743e34eef57d54cbfd5317f47d91
Jared Kramer <jared.kramer@gmail.com>
2022-09-28 15:59:42 -0700

    process hour ranges into tuples

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+SEASON_PATH = "data/season_definitions.csv"
+TIER_PATH = "data/tier_definitions.csv"
+TIMES_PATH = "data/time_of_use_definitions.csv"
+
+def process_hour_ranges(raw_strings: str):
+    #creates a list of strings with hour ranges like "7-11"
+    range_strings = raw_strings.replace(" ", "").split(";")
+    
+    #for each range, create a tuple of the integer values like (7, 11)
+    ranges = [tuple([int(rs.split("-")[0]), int(rs.split("-")[1])])
+              for rs in range_strings]
+    return ranges
+
+
+times_table = pd.read_csv(TIMES_PATH)
+times_table["Time Segments"] = times_table["Time Segments"].apply(process_hour_ranges)
+
