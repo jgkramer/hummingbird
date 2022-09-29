5c68df627c86a327182c3294e7d1bca2bec5810a
Jared Kramer <jared.kramer@gmail.com>
2022-09-28 21:28:01 -0700

    organize times table into TimesData class

--- fetch_rates.py
+++ fetch_rates.py
@@ -1,31 +1,30 @@
 import numpy as np
 import pandas as pd
 
-TIMES_PATH = "data/time_of_use_definitions.csv"
+RATES_PATH = "data/electricity_rates.csv"
 
+class RatesData:
 
-class TimesData:
+    rates_table
 
-    def __init__(self):
-        self.table = pd.read_csv(TIMES_PATH)
-        self.table["Time Segments"] = self.table["Time Segments"].apply(TimesData.process_hour_ranges)
+    def __init__
+        rates_table = pd.read_csv(RATES_PATH)
 
+    
     def process_hour_ranges(raw_strings: str):
         #from input like "7-11; 20-24", creates a list of strings with hour ranges like "7-11"
         range_strings = raw_strings.replace(" ", "").split(";")
-
+    
         #for each range, create a tuple of the integer values like (7, 11)
         ranges = [tuple([int(rs.split("-")[0]), int(rs.split("-")[1])])
-                  for rs in range_strings if rs != ""]
-
+                  for rs in range_strings]
+    
         #return a list of tuples corresponding to the original input: [(7, 11), (20, 24)]
         return ranges
- 
 
-    def print_key_columns(self):
-        print(self.table[["State", "Season", "Plan Name", "Time Period", "Time Segments"]])
 
+    
 
 if __name__ == "__main__":
-    td = TimesData()
-    td.print_key_columns()
+    times_table["Time Segments"] = times_table["Time Segments"].apply(process_hour_ranges)
+    print(times_table[["State", "Season", "Plan Name", "Time Period", "Time Segments"]])
