cce529fc5579ee96fd7b5c100e0ed98657800eea
Jared Kramer <jared.kramer@gmail.com>
2022-11-13 13:11:14 -0800

    set up reading San Diego data sheet

--- specificHourlyUsage.py
+++ specificHourlyUsage.py
@@ -17,26 +17,40 @@ from typing import List
 @dataclass(frozen = True)
 class UsagePaths:
     NV_Kramer = "usage_data/Aug21-Oct22energy.csv"
-    SD_Marshall = "usage_data/Marshall_SanDiego_11-1-2021_11_11-2022_2022.csv"
+    SD_Marshall = "usage_data/Marshall_SanDiego_11-1-2021_11-11-2022_2022.csv"
 
 
 class NVenergyUsage(HourlyEnergyUsage):
     def process_table(self, usage_path):
-        self.table = pd.read_csv(usage_path)
-        self.table["startDateTime"] = self.table["startTime"].apply(lambda s: (datetime.strptime(s, "%Y-%m-%d %H:%M:%S")))
+        table = pd.read_csv(usage_path)
+        table["startDateTime"] = table["startTime"].apply(lambda s: (datetime.strptime(s, "%Y-%m-%d %H:%M:%S")))
     
-        self.table = self.table[["startDateTime", "Usage"]]
+        self.table = table[["startDateTime", "Usage"]].copy()
         self.first_date = min(self.table["startDateTime"])
         self.last_date = max(self.table["startDateTime"])
 
+
+class SDenergyUsage(HourlyEnergyUsage):
+    def process_table(self, usage_path):
+        table = pd.read_csv(usage_path)
+        combined_start = [" ".join([d, t]) for d, t in zip(table["Date"], table["Start Time"])]
+        combined_start = [datetime.strptime(s, "%m/%d/%y %I:%M %p") for s in combined_start]
+        self.table = pd.DataFrame({"startDateTime": combined_start,
+                                   "Usage": table["Consumption"]})
+
 if __name__ == "__main__":
-    NVE = NVenergyUsage()
-    NVE.print()
+    NVE = NVenergyUsage(UsagePaths.NV_Kramer)
+#    NVE.print()
     states = ["NV"]
+
 
     s = datetime(2022, 8, 1)
     s2 = datetime(2022, 8, 31)
 
-    print(NVE.usage_by_hour_for_period(s, s2))
+#    print(NVE.usage_by_hour_for_period(s, s2))
+
+    SDE = SDenergyUsage(UsagePaths.SD_Marshall)
+    
+    
     
     
