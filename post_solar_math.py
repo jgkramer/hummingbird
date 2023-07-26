8c4e73e3273605ae6be5e11d77b1472219385722
Jared Kramer <jared.kramer@gmail.com>
2023-07-26 19:34:29 -0400

    additional edits

--- post_solar_math.py
+++ post_solar_math.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, date, timedelta
 import calendar
+import math
  
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -41,8 +42,23 @@ def plot_solar_capacity(df_solar):
                                 ymax = ymax*1.2,
                                 series_styles = series_styles,
                                 height_scale = 1.25)
-                                
-                                
+ 
+
+def cosine_beta(theta_deg, phi_deg):
+    cos_theta = math.cos(math.radians(theta_deg))
+    cos_phi = math.cos(math.radians(phi_deg))
+    return math.sqrt(1 - (cos_theta * cos_phi)**2)
+
+
+def read_solar_angles(path: str) -> pd.DataFrame:
+    df_angles_raw = pd.read_csv(path, na_values="below horizon").dropna()
+    df_angles_raw = df_angles_raw.rename(columns = {"Azimuth": "theta", "Elevation": "phi"})
+    df_angles_raw["cos B"] = df_angles_raw.apply(lambda row: cosine_beta(row["theta"], row["phi"]), axis = 1)
+    print(df_angles_raw)
+
+
+
+
 if __name__ == "__main__":
     pd.options.display.max_rows = None
 
@@ -55,5 +71,7 @@ if __name__ == "__main__":
 
     print(df_solar["Jun"])
     plot_solar_capacity(df_solar)
+
+    df_Dec = read_solar_angles("post8/solar_angles_2022Dec15.csv")
     
         
