2f35e4f514f9a5ffdfc47580100f0925de70b15c
Jared Kramer <jared.kramer@gmail.com>
2024-04-07 13:22:29 -0400

    adding rquests to api

--- post10.py
+++ post10.py
@@ -2,6 +2,8 @@
 import numpy
 import pandas
 import os
+import requests
+
 
 from datetime import datetime, timedelta
 
@@ -9,6 +11,12 @@ import matplotlib as mpl
 
 import os
 
+def get_data():
+    url_data = "https://api.eia.gov/v2/electricity/rto/region-sub-ba-data/data/?frequency=local-hourly&data[0]=value&facets[subba][]=DOM&start=2024-01-01T00:00:00-05:00&end=2024-02-01T00:00:00-05:00&sort[0][column]=period&sort[0][direction]=asc&offset=0&length=5000"
+
+
 if __name__ == "__main__":
-    key = os.getenv("EIA_API_KEY")
-    print(key)
+    api_key = os.getenv("EIA_API_KEY")
+    api = eia.API(api_key)
+
+    
