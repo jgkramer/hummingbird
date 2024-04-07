39b221240624e12d9037f66dd382bfdf772741dd
Jared Kramer <jared.kramer@gmail.com>
2024-04-07 11:17:30 -0400

    start post 10, eia key

---
+++ post10.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+from datetime import datetime, timedelta
+
+import os
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+if __name__ == "main":
+    key = os.getenv("EIA_API_KEY")
+    print(key)
+    
+
